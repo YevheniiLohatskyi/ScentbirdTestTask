537f5d1cff0b4046cb73e34bf00fdbafebc6c1a8
Yevhenii Lohatskyi <yevhenii.lohatskyi@innowise-group.com>
2023-04-08 13:00:47 +0300

    add Application classes
    (cherry picked from commit fcb6d3d83258c5794add28c18fd6c879afb87112)

---
+++ CovidApiService/src/main/java/com/yevhenii/covidapiservice/CovidApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yevhenii.covidapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidApiServiceApplication.class, args);
+    }
+
+}
