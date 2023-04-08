802d4fafd22e80bd66eb0cb6d05a3e1aad9913df
Yevhenii Lohatskyi <yevhenii.lohatskyi@innowise-group.com>
2023-04-08 13:06:25 +0300

    add main entry-point controller
    (cherry picked from commit edc4b4bbfe60c24c64b6acfc1650d9c63669c46d)

---
+++ ApiGateway/src/main/java/com/yevhenii/apigateway/controller/CovidController.java
@@ -0,0 +1,38 @@
+package com.yevhenii.apigateway.controller;
+
+import com.yevhenii.apigateway.model.CovidCountryStatistics;
+import com.yevhenii.apigateway.service.CountryService;
+import com.yevhenii.apigateway.service.CovidStatsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class CovidController {
+
+    private final CovidStatsService covidStatsService;
+
+    public CovidController(CovidStatsService covidStatsService) {
+        this.covidStatsService = covidStatsService;
+    }
+
+    @GetMapping("/new-cases-stats")
+    public ResponseEntity<Map<String, CovidCountryStatistics>> getNewCasesStats(
+        @RequestParam("countries") List<String> countries,
+        @RequestParam("from") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fromDate,
+        @RequestParam("to") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate toDate) {
+
+        Map<String, CovidCountryStatistics> statisticsByCountry =
+            covidStatsService.populateAndGetStatistics(countries, fromDate, toDate);
+
+        return new ResponseEntity<>(statisticsByCountry, HttpStatus.OK);
+    }
+}
