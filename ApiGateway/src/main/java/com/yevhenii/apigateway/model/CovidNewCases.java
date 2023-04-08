afbd7ead434267265698edfa77751c264d7f723e
Yevhenii Lohatskyi <yevhenii.lohatskyi@innowise-group.com>
2023-04-08 13:05:47 +0300

    add CovidNewCases model
    (cherry picked from commit b39cdba4d642e84bc1f75863e083117f581e7fe8)

---
+++ ApiGateway/src/main/java/com/yevhenii/apigateway/model/CovidNewCases.java
@@ -0,0 +1,40 @@
+package com.yevhenii.apigateway.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "covid_new_cases")
+public class CovidNewCases {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "country_id", nullable = false)
+    private Country country;
+
+    private LocalDate date;
+
+    private Integer newCases;
+
+    public CovidNewCases(Country country, LocalDate date, Integer newCases) {
+        this.country = country;
+        this.date = date;
+        this.newCases = newCases;
+    }
+}
